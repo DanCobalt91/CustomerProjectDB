745d182b0f00813523d3559e380230c58d49d413
DanCobalt91 <dtaylor@cobaltis.co.uk>
2025-09-15 13:21:57 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,8 @@
-base: '/'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Update base for GitHub Pages repo deployment
+export default defineConfig({
+  base: '/CustomerProjectDB/',
+  plugins: [react()],
+})
